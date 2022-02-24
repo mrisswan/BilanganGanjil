faddbfe3439dc43227732b616532b0d6e2dbf5a6
MRiswanFauzi <99298203+mrisswan@users.noreply.github.com>
2022-02-24 10:31:45 +0700

    Add files via upload

---
+++ BilanganGanji/src/bilanganganji/BilanganGanji.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bilanganganji;
+
+public class BilanganGanji {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Bilangan ganjil 0-20");  
+        
+      for(int i=0; i <= 20; i++){
+          if (i%2!=0)
+            System.out.print(i+",");
+      }
+        
+    }
+    
+}
